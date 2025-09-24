9f6bbe1bf2327e35f3580098dd203045f63cbb99
Nikolay Pomogaev <klagefurt@gmail.com>
2025-09-24 22:36:40 +0300

    Added a MainWindowVM

---
+++ Bookinist-Store/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,27 @@
+﻿using MathCore.WPF.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookinist_Store.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+		/// <summary>
+		/// Title
+		/// </summary>
+		private string _title;
+
+		public string Title
+		{
+			get { return _title; }
+			set { _title = value; }
+		}
+
+
+
+
+	}
+}
