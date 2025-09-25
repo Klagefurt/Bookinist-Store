803612c95113a9fac985f9060fb945a03f27cd14
Nikolay Pomogaev <klagefurt@gmail.com>
2025-09-25 22:01:12 +0300

    Container of services

--- Bookinist-Store/App.xaml.cs
+++ Bookinist-Store/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Configuration;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Configuration;
 using System.Data;
 using System.Windows;
 
@@ -9,6 +11,32 @@ namespace Bookinist_Store
     /// </summary>
     public partial class App : Application
     {
+        private static IHost _Host;
+
+        public static IHost Host => _Host 
+            ??= Program.CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
+
+        public static IServiceProvider Services => Host.Services;
+
+        internal static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
+        {
+            
+        }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            var host = Host;
+            base.OnStartup(e);
+            await host.StartAsync();
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            using var host = Host;
+            base.OnExit(e);
+            await Host.StopAsync();
+        }
+
     }
 
 }
---
+++ Bookinist-Store/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookinist_Store
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var app = new App();
+            app.InitializeComponent();
+            app.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) => Host
+            .CreateDefaultBuilder(args)
+            .ConfigureServices(App.ConfigureServices)
+            ;
+    }
+}
